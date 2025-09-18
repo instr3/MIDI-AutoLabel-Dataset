c288f53ea828c36de22e7c882eb60e30349fbd69
Junyan <instr3@163.com>
2025-09-18 17:31:24 +0800

    Added settings.py

---
+++ settings.py
@@ -0,0 +1,5 @@
+LA_DATASET_PATH = r'data/Los-Angeles-MIDI-Dataset-Ver-4-0-CC-BY-NC-SA'
+RWC_DATASET_PATH = r'data/RWC-POP'
+NOTTINGHAM_DATASET_PATH = r'data/nottingham-dataset'
+
+SOUNDFONT_PATH = r'data/FluidR3_GM.sf2'
